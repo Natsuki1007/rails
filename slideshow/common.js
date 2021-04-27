640a8939f6a52374cc483e220a349171d76c1941
Natsuki1007 <51719748+Natsuki1007@users.noreply.github.com>
2021-04-27 11:08:26 +0900

    Add files via upload

---
+++ slideshow/common.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('.slick01').slick({
+    prevArrow:'<button class="slide-arrow prev-arrow"></button>',
+    nextArrow:'<button class="slide-arrow next-arrow"></button>',
+    autoplay: true,
+    autoplayspeed: 4000
+  });
+});
